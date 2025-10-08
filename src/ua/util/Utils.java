479f07492348544f158114afba2db5bfa4e57d82
Vitalina-ts <vitalina.tsykatiak@gmail.com>
2025-10-08 09:08:18 +0300

    Create Utils.java

---
+++ src/ua/util/Utils.java
@@ -0,0 +1,10 @@
+package ua.util;
+
+import ua.model.Player;
+
+public class Utils {
+
+    public static void printPlayerInfo(Player player) {
+        System.out.println("Інформація про гравця: " + player);
+    }
+}
