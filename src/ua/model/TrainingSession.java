6b79f8e94fe30983ec173ef6064efe30e27e8350
Vitalina-ts <vitalina.tsykatiak@gmail.com>
2025-10-08 09:32:15 +0300

    Update TrainingSession.java

--- src/ua/model/TrainingSession.java
+++ src/ua/model/TrainingSession.java
@@ -2,23 +2,15 @@ package ua.model;
 
 import java.time.LocalDateTime;
 
-public class TrainingSession {
-    private LocalDateTime sessionDateTime;
-    private int duration;
-    protected Team team; 
+public record TrainingSession(LocalDateTime sessionDateTime, int duration, Team team) {
 
-    public TrainingSession(LocalDateTime sessionDateTime, int duration, Team team) {
-        this.sessionDateTime = sessionDateTime;
-        this.duration = duration;
-        this.team = team;
-    }
-
-    public static TrainingSession of(LocalDateTime sessionDateTime, int duration, Team team) {
-        return new TrainingSession(sessionDateTime, duration, team);
+    public static TrainingSession create(LocalDateTime dt, int duration, Team team) {
+        return new TrainingSession(dt, duration, team);
     }
 
     @Override
     public String toString() {
-        return String.format("Training on %s (%d min) for %s", sessionDateTime, duration, team.name());
+        return "Тренування команди " + team.name() + 
+               " (" + sessionDateTime + ", " + duration + " хв)";
     }
 }
